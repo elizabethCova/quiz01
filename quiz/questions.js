973f8427e486dd66620a80ebaecc48149e47a578
elizabethCova <129977592+elizabethCova@users.noreply.github.com>
2023-05-15 13:56:12 -0600

    Add files via upload

---
+++ quiz/questions.js
@@ -0,0 +1,70 @@
+let questions = [
+    {
+    numb: 1,
+    question: "¿En una reacción química se pierden átomos?",
+    answer: "No, solo cambia el tipo de enlace",
+    options: [
+      "Sí, para que lleguen nuevos",
+      "No, solo cambia el tipo de enlace",
+      "No, solo se agregan nuevos",
+      "Sí, para convertirse en algo nuevo"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Dependiendo de la logitud de onda...",
+    answer: "Podemos ver distintos colores",
+    options: [
+      "Podemos decir la temperatura de un objeto",
+      "Podemos distinguir los objetos",
+      "Podemos escuchar distintas frecuencias",
+      "Podemos ver distintos colores"
+    ]
+  },
+    {
+    numb: 3,
+    question: "La longitud de onda del color verder",
+    answer: "570 nanómetros",
+    options: [
+      "570 nanómetros",
+      "300 nanómetros",
+      "250 nanómetros",
+      "720 nanómetros"
+    ]
+  },
+    {
+    numb: 4,
+    question: "¿Qué se libera cuando se quema sulfato cúprico?",
+    answer: "fotones",
+    options: [
+      "quartz",
+      "calor",
+      "electricidad",
+      "fotones"
+    ]
+  },
+    {
+    numb: 5,
+    question: "¿Qué elemento además de el cobre se ve de color verde al estarse quemando?",
+    answer: "Boro",
+    options: [
+      "Boro",
+      "Ácido súlfurico",
+      "Galio",
+      "Ácido acético"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
